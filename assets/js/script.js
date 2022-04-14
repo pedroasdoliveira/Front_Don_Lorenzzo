64836fd92bd9a87f4b303130a6da8ccb43b96e5a
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-04-13 21:43:14 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+const baseURL = "http://localhost:3002/pizzas"
+
+// Get all --------------------------------------------------
+async function findAllPizzas() {
+    const response = await fetch(`${baseURL}/catalogo-pizzas`);
+    const pizzas = await response.json();
+
+    pizzas.forEach(function (pizza) {
+        document.querySelector('#pizzaList').insertAdjacentHTML('beforeend',
+            `
+        <div class="PizzaListaItem">
+
+          <div>
+            <div class="PizzaListaItem__numero">#${pizza.id}</div>
+            <div class="PizzaListaItem__sabor">${pizza.sabor}</div>
+            <div class="PizzaListaItem__preco">R$ ${pizza.preco}</div>
+            <div class="PizzaListaItem__descricao">${pizza.descricao}</div>
+            <div class="PizzaListaItem__avaliacao">${pizza.avaliacao}⭐</div>
+          </div>
+            <img class="PizzaListaItem__foto" src="${pizza.image}" width="50%" alt="Pizza de ${pizza.sabor}"/>
+        </div>
+        `)
+    });
+}
+
+// Get By Id -------------------------------------------------
+async function findByIdPizza() {
+    const idPizza = document.querySelector('#idPizza').value;
+
+    const response = await fetch(`${baseURL}/pizza/${idPizza}`);
+    const pizza = await response.json();
+
+    const divPizzaEscolhida = document.querySelector('#pizzaEscolhida');
+
+    divPizzaEscolhida.innerHTML =
+        `
+    <div class="pizzaCardItem">
+        <div>
+        <div class="pizzaCardItem__numero">#${pizza.id}</div>
+        <div class="pizzaCardItem__sabor">${pizza.sabor}</div>
+        <div class="pizzaCardItem__preco">R$ ${pizza.preco}</div>
+        <div class="pizzaCardItem__descricao">${pizza.descricao}</div>
+        <div class="pizzaCardItem__avaliacao">${pizza.avaliacao}⭐</div>
+        </div>
+        <img class="pizzaCardItem__foto" src="${pizza.image}" width="50%" alt="Pizza de ${pizza.sabor}"/>
+    </div>
+    `
+}
+
+findAllPizzas();
