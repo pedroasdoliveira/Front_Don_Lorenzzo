9c3cff264472269b4208fd6926373b9c34c48ac3
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-04-17 18:06:45 -0300

    create and update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const baseURL = "http://localhost:3002/pizzas"
 
-// Get all --------------------------------------------------
+// -------------------------------------------------------- Get All -------------------------------------
 async function findAllPizzas() {
     const response = await fetch(`${baseURL}/catalogo-pizzas`);
     const pizzas = await response.json();
@@ -8,7 +8,7 @@ async function findAllPizzas() {
     pizzas.forEach(function (pizza) {
         document.querySelector('#pizzaList').insertAdjacentHTML('beforeend',
             `
-        <div class="PizzaListaItem">
+        <div class="PizzaListaItem" id="PizzaListaItem_${pizza.id}">
 
           <div>
             <div class="PizzaListaItem__numero">#${pizza.id}</div>
@@ -16,14 +16,18 @@ async function findAllPizzas() {
             <div class="PizzaListaItem__preco">R$ ${pizza.preco}</div>
             <div class="PizzaListaItem__descricao">${pizza.descricao}</div>
             <div class="PizzaListaItem__avaliacao">${pizza.avaliacao}⭐</div>
-          </div>
+                <div class="PizzaListaItem__btns">
+                    <button class="edit btn" onclick="openModal(${pizza.id})">Editar</button>
+                    <button class="delete btn">Apagar</button>
+                </div>
+            </div>
             <img class="PizzaListaItem__foto" src="${pizza.image}" width="50%" alt="Pizza de ${pizza.sabor}"/>
         </div>
         `)
     });
 }
 
-// Get By Id -------------------------------------------------
+// ---------------------------------------------------- Get By Id -----------------------------------
 async function findByIdPizza() {
     const idPizza = document.querySelector('#idPizza').value;
 
@@ -39,13 +43,17 @@ async function findByIdPizza() {
 
     divPizzaEscolhida.innerHTML =
         `
-    <div class="pizzaCardItem">
+    <div class="pizzaCardItem" id="PizzaListaItem_${pizza.id}">
         <div>
         <div class="pizzaCardItem__numero">#${pizza.id}</div>
         <div class="pizzaCardItem__sabor">${pizza.sabor}</div>
         <div class="pizzaCardItem__preco">R$ ${pizza.preco}</div>
         <div class="pizzaCardItem__descricao">${pizza.descricao}</div>
         <div class="pizzaCardItem__avaliacao">${pizza.avaliacao}⭐</div>
+        </div>
+        <div class="PizzaListaItem__btns">
+            <button class="edit btn" onclick="openModal(${pizza.id})">Editar</button>
+            <button class="delete btn">Apagar</button>
         </div>
         <img class="pizzaCardItem__foto" src="${pizza.image}" width="50%" alt="Pizza de ${pizza.sabor}"/>
     </div>
@@ -55,7 +63,26 @@ async function findByIdPizza() {
 findAllPizzas();
 
 // ------------------------------------------------------- Modal -----------------------------------------------
-function openModal() {
+async function openModal(id = null) {
+    if (id != null) {
+        document.querySelector('#header-modal').innerText = 'Editar Pizza';
+        document.querySelector('#button-modal').innerText = 'Editar';
+
+        const response = await fetch(`${baseURL}/pizza/${id}`);
+        const pizza = await response.json();
+        
+        document.querySelector('#id').value = pizza.id;
+        document.querySelector('#sabor').value = pizza.sabor;
+        document.querySelector('#preco').value = pizza.preco;
+        document.querySelector('#descricao').value = pizza.descricao;
+        document.querySelector('#avaliacao').value = pizza.avaliacao;
+        document.querySelector('#image').value = pizza.image;
+    }
+    else {
+        document.querySelector('#header-modal').innerText = 'Adicionar nova pizza';
+        document.querySelector('#button-modal').innerText = 'Adicionar';
+    }
+
     const modal = document.querySelector('#overlay');
     modal.style.display = 'flex';
 }
@@ -63,4 +90,96 @@ function openModal() {
 function closeModal() {
     const modal = document.querySelector('#overlay');
     modal.style.display = 'none';
+
+    document.querySelector('#sabor').value = '';
+    document.querySelector('#preco').value = 0;
+    document.querySelector('#descricao').value = '';
+    document.querySelector('#avaliacao').value = 0;
+    document.querySelector('#image').value = '';
+
+}
+
+// ---------------------------------------------- Alert -----------------------------------------
+function openMessage() {
+    const alert = document.querySelector('#alert');
+    alert.style.display = 'flex';
+
+    setTimeout(closeMessage(), 5000);
+}
+
+function closeMessage() {
+    const alert = document.querySelector('#alert');
+    alert.style.display = 'none';
+}
+
+// ----------------------------------------- Modal create and update ------------------------------
+async function addPizza(event) {
+    event.preventDefault();
+
+    const id = document.querySelector('#id').value;
+    const sabor = document.querySelector('#sabor').value;
+    const preco = document.querySelector('#preco').value;
+    const descricao = document.querySelector('#descricao').value;
+    const avaliacao = document.querySelector('#avaliacao').value;
+    const image = document.querySelector('#image').value;
+
+    const pizza = {
+        id,
+        sabor,
+        preco,
+        descricao,
+        avaliacao,
+        image,
+    };
+
+    const modo = id > 0;
+
+    const endpoint = baseURL + (modo ? `/updated/${id}` : `/create`); // ------------ operação ternaria
+
+    const response = await fetch(endpoint, {
+        method: modo ? 'put' : 'post',
+        headers: {
+            "content-Type": "application/json",
+        },
+        mode: 'cors',
+        body: JSON.stringify(pizza),
+    });
+
+    const newPizza = await response.json();
+
+    const html = `
+    <div class="PizzaListaItem" id="PizzaListaItem_${pizza.id}">
+
+        <div>
+            <div class="PizzaListaItem__numero">#${newPizza.id}</div>
+            <div class="PizzaListaItem__sabor">${newPizza.sabor}</div>
+            <div class="PizzaListaItem__preco">R$ ${newPizza.preco}</div>
+            <div class="PizzaListaItem__descricao">${newPizza.descricao}</div>
+            <div class="PizzaListaItem__avaliacao">${newPizza.avaliacao}⭐</div>
+            <div class="PizzaListaItem__btns">
+                <button class="edit btn" onclick="openModal(${pizza.id})">Editar</button>
+                <button class="delete btn">Apagar</button>
+            </div>
+        </div>
+        <img class="PizzaListaItem__foto" src="${newPizza.image}" width="50%" alt="Pizza de ${newPizza.sabor}"/>
+    </div>
+    `;
+
+    if (modo) {
+        document.querySelector(`#PizzaListaItem_${id}`).outerHTML = html
+    }
+    else {
+        document.querySelector('#pizzaList').insertAdjacentHTML('beforeend', html);
+    }
+
+    closeModal();
+};
+
+// ---------------------------------------------- Modal delete -----------------------------------
+function openModalDelete() {
+    
+}
+
+function closeModalDelete() {
+
 }
